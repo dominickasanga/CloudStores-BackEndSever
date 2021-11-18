a4df4faaff5e80ecec326a75d8c72dedf5bfffed
dominickasanga <dominickasanga@gmail.com>
2021-11-18 06:45:31 +0200

    json web tokens

--- src/Controllers/AuthenticationController.js
+++ src/Controllers/AuthenticationController.js
@@ -12,6 +12,38 @@ module.exports = {
             })
         }
             
-        } 
+        },
+    async login (req, res) {
+        
+        try {
+            const {email, password} = req.body
+            const user = await User.findOne({
+                where: {
+                    email: email
+                }
+            })
+            if(!user) {
+               return res.status(403).send({
+                 error: 'The login information was incorrect'
+                })
+            }
+
+            const isPasswordValid = password === user.password
+            if (!isPasswordValid) {
+                return res.status(403).send({
+                  error: 'The login information was incorrect'
+                })
+            }
+
+            const userJson = user.toJSON()
+            res.send({
+                user: userJson
+            })
+        } catch (err) {
+            res.status(500).send({
+                error: 'An error has occured trying to log in'
+            })
+        }
+    }
     }
     
