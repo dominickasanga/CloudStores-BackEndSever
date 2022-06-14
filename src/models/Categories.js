80bfac245eb527f93a34e80469325227720ea3aa
dominickasanga <dominickasanga@gmail.com>
2022-06-14 12:29:01 +0200

    added action and active to categories model

--- src/models/Categories.js
+++ src/models/Categories.js
@@ -2,7 +2,9 @@ module.exports = (sequelize, DataTypes) => {
     const Category = sequelize.define('Category', {
         name: DataTypes.STRING,
         description: DataTypes.STRING,
-        categoryImageUrl: DataTypes.STRING
+        categoryImageUrl: DataTypes.STRING,
+        action: DataTypes.STRING,
+        active: DataTypes.STRING
     })
 
     return Category
